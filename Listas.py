d562b2d45f2c9436317b8696e17f84ad8a8b79bb
DaniloS135 <danilosouza.s135@gmail.com>
2025-02-19 14:55:46 -0300

    Listas

---
+++ Listas.py
@@ -0,0 +1,12 @@
+lista_numeros = [1, 2, 3]
+
+print (lista_numeros [0])
+print (lista_numeros [1])
+print (lista_numeros [2])
+
+numeros = [10, 9, 8, 7, 6]
+
+print("total:", len(numeros) )
+print("menor valor:", min(numeros))
+print("maio valor", max(numeros) )
+
