86772de2df3c97879afe34e9c1b69fc7c74a7ab2
DaniloS135 <danilosouza.s135@gmail.com>
2025-02-20 17:48:09 -0300

    Outros tipos de estruturas

--- Estruturas.py
+++ Estruturas.py
@@ -1,3 +1,4 @@
+"""
 try:
     idade = int(input("Qual a sua idade?").strip())
     if idade >= 18:
@@ -5,4 +6,19 @@ try:
     else:
             print("Você é menor de idade.")
 except ValueError:
-    print("Por favor, digite um número válido.")
+    print("Por favor, digite um número válido.")"""
+
+nota = int(input("Qual sua nota? "))
+
+if nota>= 90:
+    print ("Excelente")
+
+elif nota >= 80:
+    print ("Muito bom")
+
+elif nota >= 70:
+    print ("Bom")
+
+else:
+    print ("Precisa melhorar")
+    
