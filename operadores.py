e931dc5dc6a3c49158c904e3ee1469ee0266848b
DaniloS135 <danilosouza.s135@gmail.com>
2025-02-20 17:19:51 -0300

    Operadores de comparação

--- operadores.py
+++ operadores.py
@@ -1,3 +1,4 @@
+"""
 soma = 1 +1
 multiplicacao = 4*4
 divisao = 30/3
@@ -7,4 +8,23 @@ print("soma", soma)
 print("multiplicacao", multiplicacao)
 print("divisao", divisao)
 print("potencia", potencia)
+"""
 
+#a = 10, b = 3
+
+a = 10
+b = 3
+
+igual = a == b
+diferente = a != b
+maior_que = a > b
+menor_que = a < b
+maior_ou_igual = a>= b
+menor_ou_igual = a <= b
+
+print igual
+print diferente
+print maior_que
+print menor_que
+print maior_ou_igual
+print menor_ou_igual
