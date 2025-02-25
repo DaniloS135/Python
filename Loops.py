1c6cbc7b27f618b6f6ad4b40ec2deb5676b81854
DaniloS135 <danilosouza.s135@gmail.com>
2025-02-25 01:48:41 -0300

    Loops

---
+++ Loops.py
@@ -0,0 +1,39 @@
+"""For
+
+frutas = ["maçã", "banana", "laranja"]
+
+for fruta in frutas:
+    print(fruta)
+    """
+
+
+"""While
+
+contador = 0
+
+while True:
+    print(contador)
+    contador += 1
+
+    if contador == 5:
+        break
+"""        
+
+"""while contador < 7:
+    print(contador)
+    contador += 1
+
+    if contador == 5:
+        break
+"""
+
+"""#Continue
+    
+for i in range(10):
+    if i % 2 == 0:
+        continue
+    print(i)
+"""
+
+for i in range(5):
+    pass
