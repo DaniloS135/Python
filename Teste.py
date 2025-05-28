93d23240995aa90be0c9594e46e3ce495ce911df
DaniloS135 <danilosouza.s135@gmail.com>
2025-05-28 02:51:51 -0300

    Teste do Curso

---
+++ Teste.py
@@ -0,0 +1,19 @@
+
+#numero = float(input ("Informe o número:"))
+
+#if numero % 2 == 0:
+#           print("Par")
+           
+#else: 
+#    print("Impar")
+    
+
+def multiplicar (a,b):
+    return  a*b
+
+resultado = multiplicar(5,3) + multiplicar (2,4)    
+
+
+    
+
+
