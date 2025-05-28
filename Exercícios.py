fe214247fa7ba7d439c775083200562d86c4f728
DaniloS135 <danilosouza.s135@gmail.com>
2025-05-28 03:12:03 -0300

    Exercícios GPT

---
+++ Exercícios.py
@@ -0,0 +1,25 @@
+dados = [10,20,30]
+dados.append(40)
+print(dados[2])
+#vai imprimir 30
+
+
+#valores = (1,2,3)
+#valores [0] = 10
+#print(valores)
+#vai gerar erro
+
+pessoa = {"nome" : "ana", "idade" : 30}
+
+print(pessoa.get("nome"))
+
+conjunto = {1, 2, 3, 2, 1}
+print(conjunto)
+
+lista = [5, 10, 15]
+lista[1] = lista[1] + 5
+print(lista)
+
+
+#Acertei 5 de 5
+
