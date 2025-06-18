25936ee2d4f3ffebc4c9a907b3ce3db2820a519d
DaniloS135 <danilosouza.s135@gmail.com>
2025-06-18 19:15:52 -0300

    Pacotes de módulos

---
+++ Pacotes.py
@@ -0,0 +1,9 @@
+#Para criar um pacote, criamos um diretório com o nome desejado e adicionamos um arquivo especial chamado __init__.py dentro do diretório. Este arquivo pode estar vazio ou conter código de inicialização do pacote.
+
+from meu_pacote import modulo1, modulo2
+
+modulo1.funcao()
+modulo2.funcao()
+
+#A importação e criação de módulos e pacotes em Python nos permite organizar e reutilizar nosso código de maneira eficiente. Ao modularizar nosso código, podemos manter um código mais legível, estruturado e fácil de manter.
+
--- Teste.py
+++ Teste.py
@@ -14,6 +14,12 @@ def multiplicar (a,b):
 resultado = multiplicar(5,3) + multiplicar (2,4)    
 
 
+x = 5
+y = "3"
+z = x + int(y)
+print(z)
+
+print((not True) or (False and True))
     
 
 
