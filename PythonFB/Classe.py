2dd8d52d379e7e39728c6ac54cf68cfbfbc8aa9e
DaniloS135 <danilosouza.s135@gmail.com>
2025-06-25 00:56:44 -0300

    Classes e usos básicos.

---
+++ PythonFB/Classe.py
@@ -0,0 +1,13 @@
+class Pessoa:
+    "Isto é uma nova classe chamada Pessoa"
+    idade = 15
+    
+    def saudacao(self):
+        print("Olá Pessoas!")
+
+print(Pessoa.idade)
+
+print(Pessoa.saudacao)
+
+print(Pessoa.__doc__)
+
