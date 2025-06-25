baee37588148f9102c23811cdb3a4ada8ffa4ec0
DaniloS135 <danilosouza.s135@gmail.com>
2025-06-25 01:15:58 -0300

    Praticando o uso da Classe a partir da criação da class Pessoa onde pergunto o nome e a idade da pessoa e retorno a pertir de uma outra função uma mensagem de saudação para ela.

--- PythonFB/Classe.py
+++ PythonFB/Classe.py
@@ -1,13 +1,20 @@
 class Pessoa:
     "Isto é uma nova classe chamada Pessoa"
-    idade = 15
     
+    def __init__(self, nome):
+        self.nome = nome
+        self.idade = input("Digite a sua idade:")
+        
     def saudacao(self):
-        print("Olá Pessoas!")
+        print(f"Olá {self.nome}!")
 
-print(Pessoa.idade)
+'''print(Pessoa.idade)
 
 print(Pessoa.saudacao)
 
-print(Pessoa.__doc__)
+print(Pessoa.__doc__)'''
+
+nome_input = input("Digite seu nome:")
+pessoa = Pessoa(nome_input)
+pessoa.saudacao()
 
